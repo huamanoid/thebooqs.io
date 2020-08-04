56c8a5cbda33c183160738b5ebe512617203e839
Aman Kumar <51534408+huamanoid@users.noreply.github.com>
2020-08-05 00:11:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8000;
+
+// app.engine('html', require('ejs').renderFile);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('home.ejs');
+});
+
+app.get('/blog', function(req, res) {
+	res.render('blog.ejs');
+});
+
+app.listen(port, () => {
+	console.log('listening on port 8000');
+});
