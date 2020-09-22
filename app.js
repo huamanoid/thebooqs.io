a9c6c69d8d7841abe611fde5b96f3f9c6cfeeee4
thebleublood <aman18a@iitg.ac.in>
2020-09-22 22:05:36 +0530

    coming soon page added

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ var port = process.env.PORT || 8000;
 // app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
+	res.render('beta.ejs');
+});
+
+app.get('/home', function (req, res) {
 	res.render('home.ejs');
 });
 
-app.get('/blog', function(req, res) {
+app.get('/blog', function (req, res) {
 	res.render('blog.ejs');
 });
 
